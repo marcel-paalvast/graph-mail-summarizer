f3d80029617e2094a1fa8ab935a41a3b213d7b59
marcel-paalvast <126659601+marcel-paalvast@users.noreply.github.com>
2023-03-05 15:18:08 +0100

    Added Startup and DI using ISummarizer

---
+++ api/mail-summarizer-api/Services/SimpleSummarizer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mail_summarizer_api.Services;
+public class SimpleSummarizer : ISummarizer
+{
+    public Task<string> SummarizeAsync(string message)
+    {
+        var paragraphs = message.Split(new string[] { "\r\n\r\n", "\n\n" }, StringSplitOptions.RemoveEmptyEntries);
+        var builder = new StringBuilder();
+
+        foreach (var paragraph in paragraphs)
+        {
+            var sentences = paragraph.Split('.', 2);
+            builder.Append(sentences[0]);
+            builder.Append(". ");
+        }
+
+        return Task.FromResult(builder.ToString());
+    }
+}
---
+++ api/mail-summarizer-api/Startup.cs
@@ -0,0 +1,20 @@
+﻿using mail_summarizer_api.Services;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: FunctionsStartup(typeof(mail_summarizer_api.Startup))]
+namespace mail_summarizer_api;
+
+public class Startup : FunctionsStartup
+{
+    public override void Configure(IFunctionsHostBuilder builder)
+    {
+        builder.Services.AddSingleton<ISummarizer, SimpleSummarizer>();
+    }
+}
